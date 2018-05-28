5ed6be0566c8db12e673fc92d3f30a931762ba49
Josenaldo <josenaldo.asj@gmail.com>
2018-05-27 21:17:27 -0300

    client code

---
+++ SnakeClient/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import presentation.FrmBoard;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		FrmBoard frmBoard = new FrmBoard(50, 50);
+		frmBoard.setVisible(true);
+	}
+}
